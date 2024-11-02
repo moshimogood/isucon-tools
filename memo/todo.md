c00b6ff8ca2c1b88109bd7693928ad179a71152b
takahara <takaharayuuki7@gmail.com>
2024-11-02 19:42:49 +0900

    update

---
+++ memo/todo.md
@@ -0,0 +1,4 @@
+1. git 連携
+2. alp
+3. pt-query-digest
+4. db 周り
---
+++ shell/alp-init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://github.com/tkuchiki/alp/releases/download/v1.0.11/alp_linux_amd64.zip
+unzip alp_linux_amd64.zip
+sudo install ./alp /usr/local/bin
