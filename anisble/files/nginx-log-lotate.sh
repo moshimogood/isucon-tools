e88434fae7d7cecd32148ced6ce82983749f81fc
takahara <takaharayuuki7@gmail.com>
2024-11-30 17:27:10 +0900

    色々修正

--- anisble/files/nginx-log-lotate.sh
+++ anisble/files/nginx-log-lotate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # アクセスログを日時がついたファイル名に退避
-mv /var/log/nginx/access.log /var/log/nginx/access.log.`date + %Y%m%d-%H%M%S`
+sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.$(date +"%Y%m%d-%H%M%S")
 
 # access.logを読み取るように通知
-nginx -s reopen
+sudo nginx -s reopen
--- anisble/files/query-log-lotate.sh
+++ anisble/files/query-log-lotate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-# ~/isucari/logが存在していること 
-sudo pt-query-digest /var/log/mysql/mysql-slow.log > ~/isucari/log/$(date +mysql-slow.log-%m-%d-%H-%M -d "+9 hours")
+# ~/log/queryが存在していること 
+sudo pt-query-digest /var/log/mysql/mysql-slow.log > ~/log/query/$(date +mysql-slow.log-%m-%d-%H-%M -d "+9 hours")
 sudo rm /var/log/mysql/mysql-slow.log
+sudo systemctl restart mysql
